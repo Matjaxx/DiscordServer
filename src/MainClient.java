d5a455480543258ef110afb6f256c069b55adc14
Matjaxx <mateodomker@gmail.com>
2023-04-10 23:53:30 +0200

    socket client connection sur ip public de fritatos (si vous faites de la d avec mon ip jvous ddos =))

--- src/MainClient.java
+++ src/MainClient.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class MainClient {
     public static void main(String[] args) throws IOException {
-        Client client = new Client("192.168.254.1", 1234);
+        Client client = new Client("192.168.1.35", 1234);
         client.execute();
     }
 }
