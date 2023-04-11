b5d729fc18ca2d8067dd5865a5758d89fbf2eceb
Matjaxx <mateodomker@gmail.com>
2023-04-11 11:28:52 +0200

    adress ip public

--- src/MainClient.java
+++ src/MainClient.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class MainClient {
     public static void main(String[] args) throws IOException {
-        Client client = new Client("192.168.1.35", 1234);
+        Client client = new Client("86.215.36.144", 1234);
         client.execute();
     }
 }
